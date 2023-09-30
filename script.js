48fc6ec3f29e901cd9b74a8bc2368fd4c586f4f5
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2023-09-30 12:02:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+  /*alert("danger")
+    console.log("hey welcome back")
+     */
+    // amount = 1000
+    // console.log(" Your bill amount is Rs. "+ amount )
+    // let score=10
+    // console.log("The Game Score Is "+score)
+    // amount=1000
+    // console.log( "bill amount is Rs."+amount)
+    // const val=2292
+    // const val=2294
+    // console.log(val)
+    // let name = prompt("what is your name ?")
+    // console.log("hii"+ name)
+    // let tickets = prompt("number of tickets you want")
+    // console.log("you should pay",Number(tickets)*10)
+    let a = prompt("Enter a number")
+    console.log(Number(a)+10)
